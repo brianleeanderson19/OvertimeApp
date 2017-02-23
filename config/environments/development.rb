8b8c8247ff2fc3d0abff8e708e7fc4cbf1d0e030
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-02-23 14:59:04 -0700

    changed mailer files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
-  config.cache_classes = false
+ config.cache_classes = false
   config.eager_load = false
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-  config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
